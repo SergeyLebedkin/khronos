b245194e819f3b1275c1cbb0c42532922225104a
SergeyLebedkin <s.lebedkin@gmail.com>
2018-07-26 16:13:09 +0300

    Update khronos.bat

--- khronos.bat
+++ khronos.bat
@@ -1,3 +1,19 @@
+mkdir CL
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl.h',                           'CL/cl.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_d3d10.h',                     'CL/cl_d3d10.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_d3d11.h',                     'CL/cl_d3d11.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_dx9_media_sharing.h',         'CL/cl_dx9_media_sharing.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_dx9_media_sharing_intel.h',   'CL/cl_dx9_media_sharing_intel.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_egl.h',                       'CL/cl_egl.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_ext.h',                       'CL/cl_ext.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_ext_intel.h',                 'CL/cl_ext_intel.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_gl.h',                        'CL/cl_gl.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_gl_ext.h',                    'CL/cl_gl_ext.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_platform.h',                  'CL/cl_platform.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_va_api_media_sharing_intel.h','CL/cl_va_api_media_sharing_intel.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/cl_version.h',                   'CL/cl_version.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KhronosGroup/OpenCL-Headers/master/CL/opencl.h',                       'CL/opencl.h')"
+
 mkdir GL
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.khronos.org/registry/OpenGL/api/GL/glcorearb.h','GL/glcorearb.h')"
 
